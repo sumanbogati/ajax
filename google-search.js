a2909a781abb4d706a9204ffd1a8be2d77d920b4
suman <sumanbogati@gmail.com>
2017-01-14 17:25:55 +0530

    Script for custom google search
    Add script for custom google search input box for jsgyan.blogspot.com

---
+++ google-search.js
@@ -0,0 +1,13 @@
+(function() {
+	var cx = '015213596780635108127:b3p8xolkbty';
+	var gcse = document.createElement('script');
+	gcse.type = 'text/javascript';
+	gcse.async = true;
+	gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(gcse, s);
+})();
+
+window.onload = function (){
+	document.querySelector('#jsgyanAjaxExample input.gsc-input').placeholder = 'Write here anything...';
+}
